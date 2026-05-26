bae28dfa898ba69aa7294781140aa65c44d23733
Hugo Moraga <hg.moraga@gmail.com>
2026-05-25 23:23:34 -0400

    fix(textual): remove border_title/border_subtitle from Log init - not supported

--- ui_core/textual/widgets/log_panel.py
+++ ui_core/textual/widgets/log_panel.py
@@ -5,7 +5,4 @@ from textual.widgets import Log
 
 class LogPanel(Log):
     def __init__(self, **kwargs):
-        super().__init__("", id="log-section", border_title="LOG", border_subtitle="activity", **kwargs)
-
-    def write(self, message: str) -> None:
-        super().write(message, auto_scroll=True)
+        super().__init__("", id="log-section", auto_scroll=True, **kwargs)
